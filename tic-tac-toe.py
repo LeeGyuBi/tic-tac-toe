5a6c571d113d73fce2f766ef6799baa0b74a259e
gyubi <rbql7687@naver.com>
2017-11-26 22:11:45 +0900

    msg

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,23 +1,58 @@
-from tkinter import *
+from Tkinter import *
 
-def checked(i) :
+def next() :
+      global player
+
+      if player == "O":
+            player = "X"
+      else:
+            player = "O"
+
+def winner(player):
+      win = True
+
+      for flag in winnerFlag:
+            for elem in flag:
+                  win = True
+                  button = list[elem]
+                  if button["text"] != player:
+                        win = False
+                        break
+            if win:
+                  break
+      return win
+
+def disable():
+      for button in list:
+            button["state"] = "disabled"
+
+def checked(i):
       global player
       button = list[i]
 
       if button["text"] != "     " :
             return
-      button["text"] = player 
-      button["bg"] = "yellow"
+      
+      button["text"] = player
 
-      if player == "X" :
-            player = "O"
-            button["bg"] = "yellow"
-      else :
-            player = "X"
+      if player == "O" :
             button["bg"] = "lightgreen"
+      else :
+            button["bg"] = "yellow"
 
+      if winner(player):
+            print("Player " + player + " is win!")
+            disable()
+
+      next()
+      
 window = Tk()
 player = "X"
+winnerFlag = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+]
 list= []
 
 for i in range(9) :
